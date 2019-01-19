27ef9723233f5315c6f3fde60e6d6280c4c38b59
00Sparky <spatky_rush@inbox.ru>
2019-01-19 15:04:53 +0300

    Used default code format

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorldProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorldProgram.java
@@ -2,10 +2,10 @@ package ru.stqa.pft.sandbox;
 
 public class HelloWorldProgram {
 
-  public static void main (String[] args) {
+    public static void main(String[] args) {
 
-    System.out.println("Hello, World! I`m working.");
+        System.out.println("Hello, World! I`m working.");
 
-}
+    }
 
 }
