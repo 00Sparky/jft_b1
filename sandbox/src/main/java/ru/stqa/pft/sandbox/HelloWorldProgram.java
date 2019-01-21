cc7209b9b373d4baf8a3ae96adc92f37f8660d4f
00Sparky <spatky_rush@inbox.ru>
2019-01-21 23:11:46 +0300

    output modified to function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorldProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorldProgram.java
@@ -3,7 +3,11 @@ package ru.stqa.pft.sandbox;
 public class HelloWorldProgram {
 
     public static void main(String[] args) {
+        hello();
 
+    }
+
+    public static void hello() {
         System.out.println("Hello, World! I`m working.");
 
     }
