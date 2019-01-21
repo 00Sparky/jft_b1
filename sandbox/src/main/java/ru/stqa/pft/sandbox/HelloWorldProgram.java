c06d6891707c6097732e6fee692e034da096740c
00Sparky <spatky_rush@inbox.ru>
2019-01-21 23:32:44 +0300

    some functions added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorldProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorldProgram.java
@@ -3,13 +3,17 @@ package ru.stqa.pft.sandbox;
 public class HelloWorldProgram {
 
     public static void main(String[] args) {
-        hello();
+        hello("World");
+        hello("user");
 
     }
 
-    public static void hello() {
-        System.out.println("Hello, World! I`m working.");
+    public static void hello(String username) {
+        System.out.println("Hello," + username +"! I`m working.");
 
+    }
+    public static double area(double l) {
+        return l*l;
     }
 
 }
