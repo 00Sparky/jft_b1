d2dc5066d888842e0304b01cc89722fac17bc1f1
00Sparky <spatky_rush@inbox.ru>
2019-02-13 20:43:57 +0300

    Optimized text input

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1-edit", "test2-edit", "test3-edit"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2-edit", "test3-edit"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
