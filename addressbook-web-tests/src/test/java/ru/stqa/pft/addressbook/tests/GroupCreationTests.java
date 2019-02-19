4eb9a9a04bb966b5f4f850037f5403d4ab725cb3
00Sparky <spatky_rush@inbox.ru>
2019-02-19 20:12:11 +0300

    max Id finding method in group creation test changed to lambda-function

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,30 +1,33 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().initiateGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
-    List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size()+1);
-  }
+    @Test
+    public void testGroupCreation() throws Exception {
+        app.getNavigationHelper().gotoGroupPage();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().initiateGroupCreation();
+        GroupData group = new GroupData("test1", "test2", "test3");
+        app.getGroupHelper().fillGroupForm(group);
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
 
 
-
+        group.setId(after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    }
 
 
 }
