cdaa6a078df66c33bf084e4d03b2c68a74ecfd7e
00Sparky <spatky_rush@inbox.ru>
2019-03-06 00:55:24 +0300

    Task# 12,13
    Added test data generator for contacts (.xml)
    Contact creation text modified to use data from generated file.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreateNew.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreateNew.java
@@ -1,32 +1,50 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserCreateNew extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      //String[] split = line.split(";");
+      //  list.add(new Object[]{new GroupData().withGroupName(split[0]).withGroupHeader(split[1]).withGoupFooter(split[2])});
+      xml+= line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>)xstream.fromXML(xml);
+    return contacts.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
 
-  @Test
-  public void testNewUserCreation() throws Exception {
+  @Test (dataProvider = "validContacts")
+  public void testNewUserCreation(ContactData contact) throws Exception {
     app.goTo().gotoHomepage();
     Contacts before = app.contact().all();
     app.contact().goToAddNewUserPage();
     File photo = new File("src/test/resources/file.jpg");
-    ContactData contact = new ContactData()
-            .withNewUserName("name")
-            .withNewUserLastname("Fileadd")
-            .withNewUserAddress("Fill address form")
-            .withNewUserMoblle("3224441123")
-            .withNewUserEmail1("mailname@mail.do")
-            .withPhoto(photo)
-            .withGroup("test1");
     app.contact().createNewContact(contact, true);
 
     Assert.assertEquals(app.contact().count(), before.size()+1);
